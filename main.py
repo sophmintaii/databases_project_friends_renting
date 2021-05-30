c94f7a74290f87bba68b52debdad9bfc7bb97977
Mark Valyavka <v221101mark@gmail.com>
2021-05-30 19:09:43 +0300

    uncommented error pages

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def hello():
 @app.route("/actions", methods=["GET", "POST"])
 def actions():
 
-    # try:
+    try:
         celebrations_data, _ = db_perform_query("select * from party")
         rent_friends_data, _ = db_perform_query("select id, description, name, surname from friend")
         unreturned_gifts_data = get_unreturned_gifts()
@@ -31,33 +31,33 @@ def actions():
                                    unreturned_gifts_data=unreturned_gifts_data,
                                    rent_friends_data=rent_friends_data,
                                    celebrations_data=celebrations_data)
-    # except:
-    #     error_msg = "something went wrong"
-    #     return render_template("error.html", error_msg=error_msg)
+    except:
+        error_msg = "something went wrong"
+        return render_template("error.html", error_msg=error_msg)
 
 
 @app.route("/db_custom_views", methods=["GET", "POST"])
 def custom_views():
 
-    # try:
+    try:
         if request.method == "GET":
             client_data, cols = db_perform_query("select id, name, surname from user_")
             friend_data, cols = db_perform_query("select id, name, surname from friend")
             hobby_data, cols = db_perform_query("select hobby_id, name from hobby;")
             return render_template("custom_views.html", client_data=client_data, friend_data=friend_data, hobby_data=hobby_data)
-    # except:
-    #     error_msg = "something went wrong"
-    #     return render_template("error.html", error_msg=error_msg)
+    except:
+        error_msg = "something went wrong"
+        return render_template("error.html", error_msg=error_msg)
 
 @app.route("/db_user_views", methods=["GET", "POST"])
 def user_views():
 
-    # try:
+    try:
         if request.method == "GET":
             return render_template("user_views.html")
-    # except:
-    #     error_msg = "Couldn't load user views page"
-    #     return render_template("error.html", error_msg=error_msg)
+    except:
+        error_msg = "Couldn't load user views page"
+        return render_template("error.html", error_msg=error_msg)
 
 
 @app.route("/custom_query_result", methods=["GET", "POST"])
@@ -76,28 +76,28 @@ def custom_query():
 @app.route("/user_query_result", methods=["GET", "POST"])
 def user_query():
 
-    # try:
+    try:
         if request.method == "POST":
 
             user_select_output, cols = parse_user_select_form(request.form)
             return render_template("result.html", db_response=user_select_output, db_response_cols=cols)
-    # except:
-    #     error_msg = "Couldn't perform desired query"
-    #     return render_template("error.html", error_msg=error_msg)
+    except:
+        error_msg = "Couldn't perform desired query"
+        return render_template("error.html", error_msg=error_msg)
 
 
 @app.route("/handle_action", methods=["GET", "POST"])
 def handle_action():
 
-    # try:
+    try:
         if request.method == "POST":
 
             perform_db_action(request.form)
             msg = "Successfully performed desired action!"
             return render_template("result.html", msg=msg)
-    # except:
-    #     error_msg = "Couldn't load actions page"
-    #     return render_template("error.html", error_msg=error_msg)
+    except:
+        error_msg = "Couldn't load actions page"
+        return render_template("error.html", error_msg=error_msg)
 
 
 
